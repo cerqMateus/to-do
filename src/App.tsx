02ec1fdbf245f16a0b0eaa419e932f82143df61a
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-09-09 09:08:17 -0300

    feat: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
+import { Switch } from "./components/ui/switch";
+
 function App() {
-  return <></>;
+  const [switchIsOn, setSwitchIsOn] = useState(false);
+
+  useEffect(() => {
+    // Adiciona ou remove a classe 'dark' no <html>
+    const html = document.documentElement;
+    if (switchIsOn) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [switchIsOn]);
+  return (
+    <>
+      <div className="m-6 flex justify-between">
+        TASKED
+        <div>
+          <Switch
+            id="dark-light"
+            checked={switchIsOn}
+            onCheckedChange={setSwitchIsOn}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
